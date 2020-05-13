548e28fe6aa72e64819dca4208fd52bb44e56bec
metoo5566 <tulilu2014@gmail.com>
2020-05-14 01:48:37 +0800

    可以插断点了

--- src/main.lua
+++ src/main.lua
@@ -1,14 +1,23 @@
+cc.FileUtils:getInstance():setPopupNotify(false)
+-- cc.FileUtils:getInstance():addSearchPath("src/")
+-- cc.FileUtils:getInstance():addSearchPath("res/")
+local breakSocketHandle, debugXpCall = require("LuaDebugjit")("localhost", 7003)
+cc.Director:getInstance():getScheduler():scheduleScriptFunc(breakSocketHandle, 0.3, false)
+
 require "config"
 require "cocos.init"
-cc.FileUtils:getInstance():setPopupNotify(false)
 
 local function main()
     --@RefType MyApp
-    app = require("app.MyApp"):create()
-    app:run()
+    require("app.MyApp"):create():run()
 end
 
-local status, msg = xpcall(main, __G__TRACKBACK__)
+function __G__TRACKBACK__1(errMessage)
+    debugXpCall()
+    __G__TRACKBACK__(errMessage)
+end
+
+local status, msg = xpcall(main, __G__TRACKBACK__1)
 if not status then
     print(msg)
 end
