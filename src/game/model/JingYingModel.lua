aa6d468037954472dad0e4e77b1c0155a0cdb787
metoo5566 <tulilu2014@gmail.com>
2020-06-06 19:34:15 +0800

    YH

--- src/game/model/JingYingModel.lua
+++ src/game/model/JingYingModel.lua
@@ -1,24 +1,23 @@
-local JingYingModel  = {}
+local JingYingModel = {}
 JingYingModel.rawData = nil
 
 function JingYingModel.setRestNum(num)
-    JingYingModel.rawData["2"].surplusCnt = num
+    JingYingModel.rawData[2].surplusCnt = num
     --设置剩余次数
 end
 
 function JingYingModel.getRestNum()
     --剩余次数
-    return JingYingModel.rawData["2"].surplusCnt
+    return JingYingModel.rawData[2].surplusCnt
 end
 
 function JingYingModel.getCost()
-    return JingYingModel.rawData["2"].spend
+    return JingYingModel.rawData[2].spend
 end
 
 function JingYingModel.initData(data)
     JingYingModel.rawData = data
     JingYingModel.refreshGold(data.gold)
-
 end
 
 function JingYingModel.refreshGold(num)
@@ -37,32 +36,23 @@ end
 function JingYingModel.buySuccess(data)
     print("buySuccess")
     dump(data)
-   local rtnObj = data.rtnObj
-    JingYingModel.rawData["2"].surplusCnt = rtnObj.surplusCnt
-    JingYingModel.rawData["2"].buyCnt     = rtnObj.buyCnt
+    local rtnObj = data.rtnObj
+    JingYingModel.rawData[2].surplusCnt = rtnObj.surplusCnt
+    JingYingModel.rawData[2].buyCnt = rtnObj.buyCnt
     JingYingModel.refreshGold(rtnObj.gold)
-    JingYingModel.rawData["2"].spend = rtnObj.spend
+    JingYingModel.rawData[2].spend = rtnObj.spend
 end
 
 function JingYingModel.getBuyCnt()
-    return JingYingModel.rawData["2"].buyCnt
+    return JingYingModel.rawData[2].buyCnt
 end
 
--- function JingYingModel.getItemId()
---     return JingYingModel.rawData["2"].itemId
--- end
-
--- function JingYingModel.getItemNum()
---     return JingYingModel.rawData["2"].num
--- end
-
 function JingYingModel.getMaxLv()
-    return JingYingModel.rawData["1"]
+    return JingYingModel.rawData[1]
 end
 
 function JingYingModel.getLimit()
-    return JingYingModel.rawData["2"].limit
-end 	
+    return JingYingModel.rawData[2].limit
+end
 
-
-return JingYingModel 
+return JingYingModel
