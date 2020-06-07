6e5bb1741510f75bb47490e1a14ba806453641a3
metoo5566 <tulilu2014@gmail.com>
2020-06-07 11:47:35 +0800

    YH

--- src/network/gameWebsocket.lua
+++ src/network/gameWebsocket.lua
@@ -12,7 +12,7 @@ ws.maxReqNum = 99999
 
 function ws.init(cb)
     --@RefType luaIde#cc.WebSocket
-    ws.wsSendString = cc.WebSocket:create("ws://127.0.0.1:8080/ws")
+    ws.wsSendString = cc.WebSocket:create("ws://118.25.57.248:8080/ws")
     ws.wsSendString:registerScriptHandler(
         function()
             print("WEBSOCKET_OPEN")
